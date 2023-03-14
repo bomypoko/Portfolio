6221fd43483f986f3cb0f0c9df0fd3d7f89cbd2c
Ratchasri Kidking <orc_hero@hotmail.com>
2023-03-14 15:53:37 +0700

    Layout setting

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,35 @@
-import React from 'react'
+import React from "react"
+import { BrowserRouter } from "react-router-dom"
+import { Hero, About, Contact, Experience, Feedbacks, Navbar, Tech, Works, StarsCanvas } from "./components"
 
 const App = () => {
-  return (
+  
+  return ( 
+  
+  <BrowserRouter>
 
-    <div>
-      3d Developer PortFolio
-    </div>
+      <div className="relative z-0 bg-primary">
+        {/* Start Navbar, Hero Section */}
+        <div className="bg-hero-pattern bg-cover bg-center bg-no-repeat">
+          <Navbar/>
+          <Hero/>
+        </div>
+        {/* End Navbar, Hero Section */}
+
+        {/* Start Other Section */}
+        <div>
+          <About/>
+          <Experience/>
+          <Tech/>
+          <Works/>
+          <Feedbacks/>
+          <div className="relative z-0">
+            <Contact/>
+            <StarsCanvas/>
+          </div>
+        </div>
+      </div>
+    </BrowserRouter>
   )
 }
 
