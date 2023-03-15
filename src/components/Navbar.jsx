dff6da301887eb69789d3326a933451d16125a07
Ratchasri Kidking <orc_hero@hotmail.com>
2023-03-15 16:37:53 +0700

    set logic class item

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -12,13 +12,14 @@ const Navbar = () => {
   return (
 
     // Semantic Tag ( Nav )
-    <nav className={`${styles.paddingX} w-full flex items-center py-5 fixed top-0 z-20 ` }>
-      <div className='w-full flex justify-between items-center max-w-7xl mx-auto'>
+    <nav className={`${styles.paddingX} w-full flex items-center py-5 top-0 z-20 ` }>
+      
+      <div className='w-full flex justify-between items-center max-w-7xl mx-auto '>
+
           <Link to='/' className='flex items-center gap-2'  onClick={() => {setActive('')
           window.scrollTo(0,0)
           }}
           >
-
           <img src={bomlogo} alt="logo" className='w-9 h-9 object-contain' />
           <p className='text-white text-[18px] font-bolt cursor-pointer' >Ratchari Kidking <span className='sm:block hidden'>| Web Developer </span></p>
           </Link>
@@ -26,12 +27,23 @@ const Navbar = () => {
 
           <ul className='list-none hidden sm:flex flex-row gap-10'>
             {navLinks.map((link)=> (
-              <li>
+              <li key={link.id} 
+              // ถ้า active("") มีค่าเท่ากับ 3 (link.tilte)
+              className={`${active === link.title 
+                ? "text-white"
+                : "text-secondary"
+                } hover:text-white text-[18px] font-medium cursor-pointer`}
+              onClick={() => setActive(link.title)}  >
+                
                 <a href={`#${link.id}`}>{link.title}</a>
               </li>
             ))}
+            
+
 
           </ul>
+
+          
 
           
       </div>
