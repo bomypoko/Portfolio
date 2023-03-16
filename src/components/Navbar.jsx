5aa518b1991b78cd9aa512a0cd37a53693f9527a
Ratchasri Kidking <orc_hero@hotmail.com>
2023-03-16 15:42:16 +0700

    Create Toggle Menu

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -2,11 +2,12 @@ import React, { useState } from 'react'
 import { Link } from 'react-router-dom'
 import { styles } from '../style'
 import { navLinks } from '../constants'
-import { logo , menu , close , bomlogo } from '../assets'
+import {  menu , close , bomlogo } from '../assets'
 
 const Navbar = () => {
 
   const [ active , setActive ] = useState('')
+  const [ toggle , setToggle ] = useState(false)
 
 
   return (
@@ -14,12 +15,13 @@ const Navbar = () => {
     // Semantic Tag ( Nav )
     <nav className={`${styles.paddingX} w-full flex items-center py-5 top-0 z-20 ` }>
       
+      {/* Container - Start - ( Logo and Link ) */}
       <div className='w-full flex justify-between items-center max-w-7xl mx-auto '>
 
-          <Link to='/' className='flex items-center gap-2'  onClick={() => {setActive('')
-          window.scrollTo(0,0)
-          }}
-          >
+          <Link to='/' className='flex items-center gap-2' 
+          onClick={() => {setActive('')
+          window.scrollTo(0,0)}}>
+
           <img src={bomlogo} alt="logo" className='w-9 h-9 object-contain' />
           <p className='text-white text-[18px] font-bolt cursor-pointer' >Ratchari Kidking <span className='sm:block hidden'>| Web Developer </span></p>
           </Link>
@@ -28,8 +30,8 @@ const Navbar = () => {
           <ul className='list-none hidden sm:flex flex-row gap-10'>
             {navLinks.map((link)=> (
               <li key={link.id} 
-              // ถ้า active("") มีค่าเท่ากับ 3 (link.tilte)
-              className={`${active === link.title 
+
+              className={`${active === link.title
                 ? "text-white"
                 : "text-secondary"
                 } hover:text-white text-[18px] font-medium cursor-pointer`}
@@ -38,16 +40,23 @@ const Navbar = () => {
                 <a href={`#${link.id}`}>{link.title}</a>
               </li>
             ))}
-            
-
-
           </ul>
+          {/* Mobile Nav Bar */}
 
-          
+          <div className='bom1 sm:hidden flex flex-1 justify-end items-center'>
+                <img 
+                src={toggle ? close : menu} 
+                alt="menu" 
+                className='w-[28px] h-[28px] object-contain cursor-pointer'
+                onClick={() => setToggle(prev => !toggle)}
+                />
 
-          
+                
+          </div>
+
+
       </div>
-      
+      {/* Container - End  */}
     </nav>
   )
 }
