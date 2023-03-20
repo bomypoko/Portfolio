60aa2ced9824684737bd286677f14ccb9ee81d32
Ratchasri Kidking <orc_hero@hotmail.com>
2023-03-20 15:34:44 +0700

    hero section

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -8,8 +8,8 @@ const Hero = () => {
   return (
 
     <section className='relative w-full h-screen mx-auto'>
-      <div className={`${styles.paddingX}`}>
-
+      <div className={`${styles.paddingX} absolute inset-0 top-[120px] max-w-7xl flex flex-row item-start gap-5`}>
+          ratchasri
       </div>
 
     </section>
