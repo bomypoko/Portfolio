cb98c6b392e76fcf3aeb825c3dbf8a7ad109605c
Ratchasri Kidking <orc_hero@hotmail.com>
2023-03-21 18:15:58 +0700

    set computer

--- src/components/canvas/Computers.jsx
+++ src/components/canvas/Computers.jsx
@@ -1,38 +1,50 @@
-// import React, { Suspense,useEffect,useState } from 'react'
-// import { Canvas } from '@react-three/fiber'
-// import { OrbitControls,Preload,useGLTF } from '@react-three/drei'
+// import React , { Suspense  }from "react";
+// import { Canvas } from "@react-three/fiber";
+// import { OrbitControls , Preload , useGLTF } from "@react-three/drei";
 // import CanvasLoader from '../Loader'
 
 // const Computers = () => {
-
 //   const computer = useGLTF('./desktop_pc/scene.gltf')
 
 //   return (
-//     // Cover with mesh not div if you use threejs
-//     <mesh>
-//       <hemisphereLight intensity={0.15} groundColor="black"/>
-//        <pointLight intensity={1}/>
-//        <primitive 
-//        object={computer.scene}/>
-//     </mesh>
+
+//   <mesh>
+//     <hemisphereLight 
+//       intensity={0.15}
+//       groundColor="black"/>
+//     <pointLight 
+//       intensity={1}/>
+//     <spotLight 
+//       position={[ -20, 50 ,10]}
+//       angle={0.12}
+//       penumbra={1}
+//       intensity={1}
+//       castShadow
+//       shadow-mapSize={1024}/>
+//     <primitive
+//       object={computer.scene}
+//       scale={0.75}
+//       position={[0 , -3.25 , -1.5]}
+//       rotation={[-0.01, -0.2, -0.1]}/>
+
+//   </mesh>
 //   )
 // }
 
 // const ComputerCanvas = () => {
 //   return (
 //     <Canvas
-//     frameLoop='demand'
+//     frameLoop="demand"
 //     shadows
-//     camera={{ position: [20, 3, 5], fov: 25 }}
+//     camera={{position: [20, 3 , 5], fov: 25}}
 //     gl={{ preserveDrawingBuffer: true}}
 //     >
 //       <Suspense fallback={<CanvasLoader />}>
-//         <OrbitControls 
-//         enableZoom={false}
-//         maxPolarAngle={Math.PI / 2}
-//         MinPolarAngle={Math.PI / 2}
-//         />
-//         <Computers />
+//         <OrbitControls enableZoom={false}
+//           maxPolarAngle={Math.PI / 2}
+//           minPolarAngle={Math.PI / 2}
+//           />
+//           <Computers />
 //       </Suspense>
 //       <Preload all/>
 //     </Canvas>
@@ -40,3 +52,65 @@
 // }
 
 // export default Computers
+
+
+import React, { Suspense, useState } from "react";
+import { Canvas } from "@react-three/fiber";
+import { OrbitControls, Preload, useGLTF } from "@react-three/drei";
+
+import CanvasLoader from "../Loader";
+
+const Computers = ({ isMobile }) => {
+  const computer = useGLTF("./desktop_pc/scene.gltf");
+
+  return (
+    <mesh>
+      <hemisphereLight intensity={0.15} groundColor='black' />
+      <spotLight
+        position={[-20, 50, 10]}
+        angle={0.12}
+        penumbra={1}
+        intensity={1}
+        castShadow
+        shadow-mapSize={1024}
+      />
+      <pointLight intensity={1} />
+      <primitive
+        object={computer.scene}
+        scale={isMobile ? 0.7 : 0.75}
+        position={isMobile ? [0, -3, -2.2] : [0, -3.25, -1.5]}
+        rotation={[-0.01, -0.2, -0.1]}
+      />
+    </mesh>
+  );
+};
+
+const ComputersCanvas = () => {
+  const [isMobile, setIsMobile] = useState(false);
+
+  
+
+  return (
+    <Canvas
+      frameloop='demand'
+      shadows
+      dpr={[1, 2]}
+      camera={{ position: [20, 3, 5], fov: 25 }}
+      gl={{ preserveDrawingBuffer: true }}
+    >
+      <Suspense fallback={<CanvasLoader />}>
+        <OrbitControls
+          enableZoom={false}
+          maxPolarAngle={Math.PI / 2}
+          minPolarAngle={Math.PI / 2}
+        />
+        <Computers isMobile={isMobile} />
+      </Suspense>
+
+      <Preload all />
+      
+    </Canvas>
+  );
+};
+
+export default ComputersCanvas;
