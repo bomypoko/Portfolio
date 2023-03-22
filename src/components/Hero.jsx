75df476e3d2c3f37a884f906fc76a5a1baffbf2e
Ratchasri Kidking <orc_hero@hotmail.com>
2023-03-22 11:31:28 +0700

    motion div to about

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -21,6 +21,26 @@ const Hero = () => {
 
       </div>
           <ComputersCanvas/>
+
+          <div className='absolute xs:bottom-10 bottom-34 w-full flex justify-center items-center'>
+            <a href="#about">
+              <div className='w-[35px]  h-[64px] border-4 rounded-3xl border-secondary flex justify-center items-start p-2'>
+                <motion.dev 
+                  animate={{
+                    y: [0,24,0]
+                  }}
+                  transition={{
+                    duration: 1.5,
+                    repeat: Infinity,
+                    repeatType: 'loop'
+                  }}
+                  className="w-3 h-3 rounded-full bg-secondary mb-1"
+                  >
+
+                </motion.dev>
+              </div>
+            </a>
+          </div>
     </section>
   )
 }
