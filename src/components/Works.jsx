b087a8cc32d3aeb7689f73250d85b0eccedfcac1
Ratchasri Kidking <orc_hero@hotmail.com>
2023-03-28 15:35:10 +0700

    titt does work

--- src/components/Works.jsx
+++ src/components/Works.jsx
@@ -10,8 +10,18 @@ import { fadeIn,textVariant } from '../util/motion'
 
 const ProjectCard = ( { index , name , description ,tags ,image , source_code_link }) => {
   return (
-    <>
-    </>
+    <motion.div>
+      <Tilt option={{max: 45, scale:1, speed:450}} className="bg-tertiary p-5 rounded-2xl sm:w-[360px] w-full">
+          <div className='relative w-full h-[230px]'>
+              <img className='w-full h-full object-cover rounded-2xl' src={image} alt="project_image" />
+              <div className='absolute inset-0 flex justify-end m-3 card-img_hover'>
+                <div className="black-gradient w-10 h-10 rounded-full flex justify-center items-center cursor-pointer">
+                  <img src={github} alt="source code" className='w-1/2 h-1/2 object-contain' />
+                </div>
+              </div>
+          </div>
+      </Tilt>
+    </motion.div>
   )
 }
 
